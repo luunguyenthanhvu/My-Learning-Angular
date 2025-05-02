be435e3f6a0ce6d8e34d469e525785eb4dc11f9b
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2025-05-02 22:01:32 +0700

    feat: update source code

--- 02 - Bootstrapping Angular/basics/src/app/app-tasks/app-tasks.component.ts
+++ 02 - Bootstrapping Angular/basics/src/app/app-tasks/app-tasks.component.ts
@@ -1,8 +1,10 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
+  imports: [TaskComponent],
   templateUrl: './app-tasks.component.html',
 })
 export class AppTasksComponent {
---
+++ 02 - Bootstrapping Angular/basics/src/app/app-tasks/task/task.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task',
+  standalone: true,
+  templateUrl: './task.component.html',
+})
+export class TaskComponent {}
